8bef596a79d978728a459dda2455cdbe8b0c5bc8
kuangangnguoc <linda.kua29@gmail.com>
2023-05-09 17:27:17 +0300

    leson4

--- src/components/Chatbox.js
+++ src/components/Chatbox.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './Chatbox.css'
 
 const Chatbox =(props)=> (
     <ul>
--- src/index.js
+++ src/index.js
@@ -3,12 +3,35 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Login from'./components/Login';
+import Register from'./components/Register';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+
+class AppRouter extends React.Component{
+  render(){
+    return(
+      <Router>
+        <div className='app'>
+          <nav className='main-nav'>
+            <Link to="/">Home</Link>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          </nav>
+          <Switch>
+            <Route exact path="/" component={App}/>
+            <Route exact path="/login" component={Login}/>
+            <Route exact path="/register" component={Register}/>
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <AppRouter/>
 );
 
 // If you want to start measuring performance in your app, pass a function
